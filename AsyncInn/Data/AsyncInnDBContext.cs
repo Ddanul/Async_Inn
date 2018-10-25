c6c1f03a57eff5097ff8ed0316b15ee4f7b80cb0
Danul De Leon <danul.deleon@gmail.com>
2018-10-25 10:34:34 -0700

    project scaffolded and db initial migration set

---
+++ AsyncInn/Data/AsyncInnDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data
+{
+    public class AsyncInnDBContext : DbContext
+    {
+        public AsyncInnDBContext(DbContextOptions<AsyncInnDBContext> options) : base (options)
+        {
+
+        }
+    }
+}
--- AsyncInn/Startup.cs
+++ AsyncInn/Startup.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AsyncInn.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AsyncInn
@@ -13,8 +16,20 @@ namespace AsyncInn
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set;}
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<AsyncInnDBContext>(options =>
+     options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +40,16 @@ namespace AsyncInn
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles();
+
+
         }
     }
 }
